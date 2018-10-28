dc046e070a54a68ddcd27006158803d3d348802b
Ben Halverson <benhalverson33@gmail.com>
2018-10-28 02:54:54 -0700

    wip keyboard events

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ElectronService } from '../../providers/electron.service';
 @Component({
   selector: 'app-home',
@@ -7,6 +7,13 @@ import { ElectronService } from '../../providers/electron.service';
 })
 export class HomeComponent implements OnInit {
   constructor(private _electronService: ElectronService) {}
- 
-  ngOnInit() {}
+  pressedKey;
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent($event) {
+    this.pressedKey = $event.key;
+    console.log('Keyboard Key: ', $event.key);
+  }
+  ngOnInit() {
+    this.handleKeyboardEvent(Event);
+  }
 }
